4f0cdce779886714a9700340890cd56642890a44
romeupeniche <romeupeniche12@gmail.com>
2023-03-30 22:02:51 -0300

    finished!

--- src/index.js
+++ src/index.js
@@ -1,102 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./i18n/index";
-import Home from './routes/Home';
-import TimeLineContent from './routes/TimeLineContent';
-import ErrorPage from './routes/ErrorPage';
-
-import ImgRoutes from './routes/ImgRoutes';
-
-const Year2010 = () => <>{ImgRoutes()[0]}</>;
-const Year2011 = () => <>{ImgRoutes()[1]}</>;
-const Year2012 = () => <>{ImgRoutes()[2]}</>;
-const Year2013 = () => <>{ImgRoutes()[3]}</>;
-const Year2014 = () => <>{ImgRoutes()[4]}</>;
-const Year2015 = () => <>{ImgRoutes()[5]}</>;
-const Year2016 = () => <>{ImgRoutes()[6]}</>;
-const Year2017 = () => <>{ImgRoutes()[7]}</>;
-const Year2018 = () => <>{ImgRoutes()[8]}</>;
-const Year2019 = () => <>{ImgRoutes()[9]}</>;
-const Year2020 = () => <>{ImgRoutes()[10]}</>;
-const Year2021 = () => <>{ImgRoutes()[11]}</>;
+import Home from "./routes/Home";
+import TimeLineContent from "./routes/TimeLineContent";
+import ErrorPage from "./routes/ErrorPage";
+import TLPage from "./components/TLPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
-        element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: 'timeline',
-        element: <TimeLineContent />
+        path: "/timeline",
+        element: <TimeLineContent />,
       },
       {
-        path: 'timeline/2010',
-        element: <Year2010 />
+        path: "/timeline/:year",
+        element: <TLPage />,
       },
-      {
-        path: 'timeline/2011',
-        element: <Year2011 />
-      },
-      {
-        path: 'timeline/2012',
-        element: <Year2012 />
-      },
-      {
-        path: 'timeline/2013',
-        element: <Year2013 />
-      },
-      {
-        path: 'timeline/2014',
-        element: <Year2014 />
-      },
-      {
-        path: 'timeline/2015',
-        element: <Year2015 />
-      },
-      {
-        path: 'timeline/2016',
-        element: <Year2016 />
-      },
-      {
-        path: 'timeline/2017',
-        element: <Year2017 />
-      },
-      {
-        path: 'timeline/2018',
-        element: <Year2018 />
-      },
-      {
-        path: 'timeline/2019',
-        element: <Year2019 />
-      },
-      {
-        path: 'timeline/2020',
-        element: <Year2020 />
-      },
-      {
-        path: 'timeline/2021',
-        element: <Year2021 />
-      }
-    ]
-  }
-])
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-reportWebVitals();
--- src/routes/TimeLineContent.jsx
+++ src/routes/TimeLineContent.jsx
@@ -1,32 +1,35 @@
-import './css/TimeLineContent.css';
-import Img from '../components/Img';
-import imgs from '../static/imgs.json';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import "./css/TimeLineContent.css";
+import Img from "../components/Img";
+import imgs from "../static/imgs.json";
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 function TimeLineContent() {
-    const { t } = useTranslation();
-    window.scrollTo(0, 0)
-    const returnArray = 
-    Array.from(Object.keys(imgs))
-        .map(year => {
-            return (
-                <div key={year}>
-                    <Link className='link-img' to={year}>
-                        <Img className='img' link={imgs[year][1]} link2={imgs[year][2]} link3={imgs[year][3]} link4={imgs[year][4]} alt={year} />
-                    </Link>
-                </div>
-            )
-        })
-
+  const { t } = useTranslation();
+  window.scrollTo(0, 0);
+  const returnArray = Array.from(Object.keys(imgs)).map((year) => {
     return (
-        <>
-            <p className='timeline-title'>{t("timelineHeader")}</p>
-            <nav className='content' >
-                {returnArray}
-            </nav>
-        </>
-    )
+      <div key={year}>
+        <Link className="link-img" to={year}>
+          <Img
+            className="img"
+            link={imgs[year][1]}
+            link2={imgs[year][2]}
+            link3={imgs[year][3]}
+            link4={imgs[year][4]}
+            alt={year}
+          />
+        </Link>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <p className="timeline-title">{t("timelineHeader")}</p>
+      <main className="content">{returnArray}</main>
+    </>
+  );
 }
 
 export default TimeLineContent;
